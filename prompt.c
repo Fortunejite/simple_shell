73db12fa54357bb20f36fa4c764cae2582f35cbc
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-11-12 02:31:02 -0800

    ‘m’

--- prompt.c
+++ prompt.c
@@ -26,20 +26,25 @@ int built_in(char **token, list_t *env, int num, char **command)
 	if (_strcmp(token[0], "exit") == 0)
 	{
 		i = __exit(token, env, num, command);
+	}
+	/* if user types "env", print, free cmd tokens, and reprompt */
 	else if (_strcmp(token[0], "env") == 0)
 	{
 		_env(token, env);
 		i = 1;
 	}
+	/* if user types "cd" , it will change directory */
 	else if (_strcmp(token[0], "cd") == 0)
 	{
 		i = _cd(token, env, num);
 	}
+	/* if user types "setenv", create or modify linked list node */
 	else if (_strcmp(token[0], "setenv") == 0)
 	{
 		_setenv(&env, token);
 		i = 1;
 	}
+	/* if user types "setenv", remove linked list node */
 	else if (_strcmp(token[0], "unsetenv") == 0)
 	{
 		_unsetenv(&env, token);
@@ -68,11 +73,11 @@ char *ignore_space(char *str)
  */
 void ctrl_D(int i, char *command, list_t *env)
 {
-	if (i == 0)
+	if (i == 0) /* handles Ctrl+D */
 	{
-		free(command);
+		free(command); /* exit with newline if in shell */
 		free_linked_list(env);
-		if (isatty(STDIN_FILENO))
+		if (isatty(STDIN_FILENO))/* ctrl+d prints newline */
 			write(STDOUT_FILENO, "\n", 1);
 		exit(0);
 	}
@@ -93,31 +98,34 @@ int prompt(char **en)
 	env = env_linked_list(en);
 	do {
 		command_line_no++;
-		if (isatty(STDIN_FILENO))
+		if (isatty(STDIN_FILENO)) /* reprompt if in interactive shell */
 			write(STDOUT_FILENO, "$ ", 2);
 		else
 			non_interactive(env);
-		signal(SIGINT, ctrl_c);
-		command = NULL; i = 0;
-		i = get_line(&command);
-		ctrl_D(i, command, env);
+		signal(SIGINT, ctrl_c); /* makes ctrl+c not work */
+		command = NULL;
+		i = 0;
+		i = get_line(&command); /* read user's cmd in stdin */
+		ctrl_D(i, command, env); /* exits shell if ctrl-D */
 		n_command = command;
 		command = ignore_space(command);
 		n = 0;
-		while (command[n] != '\n')
+		while (command[n] != '\n') /* replace get_line's \n with \0 */
 			n++;
 		command[n] = '\0';
 		if (command[0] == '\0')
 		{
-			free(n_command); continue;
+			free(n_command);
+			continue;
 		}
-		token = NULL; token = _str_tok(command, " ");
+		token = NULL;
+		token = _str_tok(command, " ");
 		if (n_command != NULL)
 			free(n_command);
 		exit_stat = built_in(token, env, command_line_no, NULL);
 		if (exit_stat)
 			continue;
 		exit_stat = _execve(token, env, command_line_no);
-	} while (1);
+	} while (1); /* keep on repeating till user exits shell */
 	return (exit_stat);
 }
